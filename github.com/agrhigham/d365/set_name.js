c1b17e8eeb6dfbf08d20d44077dfc15876a7328b
agrhigham <ahigham93@gmail.com>
2024-06-28 17:59:13 +0100

    Moved files

--- github.com/agrhigham/d365/set_name.js
+++
@@ -1,9 +0,0 @@
-function GetSet(executionContext) {
-
-    var formContext = executionContext.getFormContext();
-    
-    var name = formContext.getAttribute("arth_name").getValue();
-    
-    formContext.getAttribute("arth_testtext").setValue(name);
-    
-    }
